caa9c601a0b7a7d203f0b54353708572ca25e07b
Hermehar <63737394+Hermehar@users.noreply.github.com>
2020-11-09 21:06:07 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,89 @@
+import logging
+
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+import json
+import requests
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+def start(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi! this bot will give you the summary the wikipedia page')
+
+
+def help_command(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+
+def echo(update: Update, context: CallbackContext) -> None:
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+def wikipen(update,context):
+
+
+     query=update.message.text
+     
+     
+     
+     url="https://wikipenapi.herokuapp.com/page/?q= "
+     
+     r=requests.get(url+query)
+     
+     ans=r.text
+     
+     result=json.loads(ans)
+     
+     a=result["details"]
+     b=a["tittle"]
+     c=a["url"]
+     details=f"The details of the page are as follows \n Tittle of the page is {b}\n url of the page is {c}"
+     update.message.reply_text(details)
+     
+     d=result["page"]
+     e=d["summary"]
+     update.message.reply_text (e)
+     f=result["suggestions"]
+     g=f["other suggestions"]
+     print(g)
+     update.message.text (f)
+
+
+def main():
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater("1312715755:AAEokQPO6jYKh24504SUX7yWWcJIBJrO_cc", use_context=True)
+
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", help_command))
+
+    # on noncommand i.e message - echo the message on Telegram
+    #dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    dispatcher.add_handler(MessageHandler(Filters.text, wikipen))
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
